c5be0be5dfe6e765f67cb79919bb47530745ec5a
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-04 21:58:56 +0900

    chore: seperate search list scraper from product controller

--- routes/controllers/product.controller.js
+++ routes/controllers/product.controller.js
@@ -1,36 +1,12 @@
 const createError = require('http-errors');
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
+const scraper = require('../../utils/scraper');
 
-const DUMMY = require('../../mock/searchList.json');
-
-const searchTargetKeyword = async keyword => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-
-  await page.goto(`https://www.fragrantica.com/search/?query=${keyword}`);
-  await page.waitForSelector('.perfumes-row');
-
-  const content = await page.content();
-  const $ = cheerio.load(content);
-
-  const items = $('.perfumes-row').children().toArray();
-  const normalizedItems = items.map(node => {
-      const imageUrl = node.firstChild.childNodes[0].attribs.src;
-      const productId = node.lastChild.firstChild.firstChild.attribs.href.split('/')[5].match(/.*(?=\.)/gm)[0];
-      const name = node.lastChild.firstChild.firstChild.children[0].data.trim();
-      return { name, productId, imageUrl };
-  });
-
-  await browser.close();
-
-  return normalizedItems;
-};
+// const DUMMY = require('../../mock/searchList.json');
 
 const getSearchList = async (req, res, next) => {
   const { keyword } = req.query;
   try {
-    const result = await searchTargetKeyword(keyword);
+    const result = await scraper.searchTargetKeyword(keyword);
     res.send(result);
     // res.send(DUMMY);
   } catch (err) {
