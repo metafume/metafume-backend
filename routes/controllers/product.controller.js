80666d6ed9fa89ee467fcf8ba1b677234b2bb0ad
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-05 01:10:52 +0900

    feat: add product/detail router for getting product detail info

--- routes/controllers/product.controller.js
+++ routes/controllers/product.controller.js
@@ -2,10 +2,11 @@ const createError = require('http-errors');
 const scraper = require('../../utils/scraper');
 
 // const DUMMY = require('../../mock/searchList.json');
+// const DUMMY_PRODUCT_DETAIL = require('../../mock/product.json');
 
 const getSearchList = async (req, res, next) => {
-  const { keyword } = req.query;
   try {
+    const { keyword } = req.query;
     const result = await scraper.searchTargetKeyword(keyword);
     res.send(result);
     // res.send(DUMMY);
@@ -15,4 +16,16 @@ const getSearchList = async (req, res, next) => {
   }
 };
 
-module.exports = { getSearchList };
+const getProductDetail = async (req, res, next) => {
+  try {
+    const { id: path } = req.query;
+    const result = await scraper.searchProductDetail(path);
+    res.send(result);
+    // res.send(DUMMY_PRODUCT_DETAIL);
+  } catch (err) {
+    console.log(err);
+    next(createError(404));
+  }
+};
+
+module.exports = { getSearchList, getProductDetail };
