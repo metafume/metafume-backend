6da8720c9257d88116fcf53dae4e59052fd124db
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-07 00:49:42 +0900

    feat: add products/recent router for getting recent view list

--- routes/controllers/product.controller.js
+++ routes/controllers/product.controller.js
@@ -1,7 +1,12 @@
 const createError = require('http-errors');
+const redisClient = require('../../loaders/db').redisClient;
+const util = require('util');
 const scraper = require('../../utils/scraper');
 
 const MATERIAL_LIST = require('../../mock/materialList.json');
+
+const smembers = util.promisify(redisClient.smembers).bind(redisClient);
+const get = util.promisify(redisClient.get).bind(redisClient);
 
 const getSearchList = async (req, res, next) => {
   try {
@@ -28,6 +33,9 @@ const getProductDetail = async (req, res, next) => {
 
     result.notes = mapImagePathToNote;
 
+    redisClient.set(result.productId, JSON.stringify(result));
+    redisClient.sadd('recentViewList', result.productId);
+
     res.send(result);
   } catch (err) {
     console.log(err);
@@ -35,4 +43,30 @@ const getProductDetail = async (req, res, next) => {
   }
 };
 
-module.exports = { getSearchList, getProductDetail };
+const getRecentViewList = async (req, res, next) => {
+  try {
+    const data = await smembers('recentViewList');
+    const promisedList = data.map(id => get(id));
+
+    let recentViewList = await (async promises => {
+      return await Promise.all(promises);
+    })(promisedList);
+
+    recentViewList = recentViewList.map(product => {
+      const parsed = JSON.parse(product);
+      return {
+        brand: parsed.brand,
+        name: parsed.name,
+        productId: parsed.productId,
+        imageUrl: parsed.imageUrl,
+      };
+    });
+
+    res.send(recentViewList);
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+module.exports = { getSearchList, getProductDetail, getRecentViewList };
