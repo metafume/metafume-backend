06bf480926515d1262989a3ca4ba3fafbc177548
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-10 19:03:56 +0900

    feat: add middleware for authorization

---
+++ routes/middlewares/authorization.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
+
+const { tokenSecretKey } = require('../../configs');
+
+exports.verifyToken = (req, res, next) => {
+  try {
+    const { user_id } = req.params;
+    const token = req.headers['x-access-token'];
+
+    const decodedUser = jwt.verify(token, tokenSecretKey);
+    if (decodedUser._id !== user_id) return next(createError(403));
+
+    req.user = decodedUser;
+    next();
+  } catch (err) {
+    console.log(err);
+    next(createError(401));
+  }
+};
