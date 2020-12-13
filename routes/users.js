0664210cdf8db43b5bb715714e0b581939880e58
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-14 01:39:38 +0900

    style: code formatting

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const userController = require('./controllers/user.controller');
-const verifyToken = require('./middlewares/authorization').verifyToken;
+const { verifyToken } = require('./middlewares/authorization');
 
 const router = express.Router();
 
