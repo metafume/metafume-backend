4a1e378eaf6bf4a1242656a7cb00546cac7fe842
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-14 20:40:47 +0900

    chore: update environment variables

--- configs/index.js
+++ configs/index.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
 
 module.exports = {
+  clientUrl: process.env.CLIENT_URL,
   port: process.env.PORT,
   tokenSecretKey: process.env.TOKEN_SECRET_KEY,
   databaseUrl: process.env.MONGO_DB_URL,
+  nodemailerUser: process.env.NODEMAILER_USER,
+  nodemailerPass: process.env.NODEMAILER_PASS,
   mongooseOptions: {
     useCreateIndex: true,
     useNewUrlParser: true,
