fec848b352c6ae7ed6e65cd195aae8cad93b94db
Hyeon Chang Shin <stgo@Hyeonui-MacBookPro.local>
2020-12-14 20:45:38 +0900

    feat: add mail service

---
+++ utils/mailer.js
@@ -0,0 +1,39 @@
+const { parentPort } = require('worker_threads');
+const nodemailer = require('nodemailer');
+const { nodemailerUser, nodemailerPass } = require('../configs');
+const { template } = require('./mailTemplate');
+
+const transportOptions = {
+  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: nodemailerUser,
+    pass: nodemailerPass,
+  },
+};
+
+const sendMail = async (receiver, keyword) => {
+  const content = template(keyword);
+
+  const transporter = nodemailer.createTransport(transportOptions);
+
+  await transporter.sendMail({
+    from: `Metafume <${nodemailerUser}>`,
+    to: receiver,
+    subject: 'METAFUME',
+    html: content,
+  });
+
+  console.log('Sent message: %s', receiver);
+};
+
+parentPort.on('message', async receivers => {
+  const promisedList =
+    receivers.map(({ email, keyword }) => sendMail(email, keyword));
+
+  await (async promises => {
+    return await Promise.all(promises);
+  })(promisedList);
+});
